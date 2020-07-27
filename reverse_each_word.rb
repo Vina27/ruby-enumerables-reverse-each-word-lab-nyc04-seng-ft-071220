5f107259a3f8d85e833dcd2fc7d273baa1c72e0d
Maysoon Tabali <mtab03@me.com>
2020-07-27 12:57:30 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,19 @@
+# def reverse_each_word(string)
+#   array = string.split(" ")
+#   string.each do |word|
+    
+  
+#   string.each {|word| word.reverse}
+#   new_string(word).join(" ")
+# end
+
+
+# def reverse_string(str)
+#   arr = str.split(" ")
+#   new_arr = arr.collect{|a| a.reverse}
+#   new_arr.join(" ")
+# end
+
+def reverse_each_word(sentence1)
+  sentence1.split.collect {|word| word.reverse.join(" ")}
+end 
